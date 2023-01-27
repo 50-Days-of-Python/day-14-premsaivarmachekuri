58a2aec712a2dd85cfe2d85528d605c25896d805
Chekuri P V S Yogananda Varma <110842297+premsaivarmachekuri@users.noreply.github.com>
2023-01-27 22:41:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,5 @@ def flatten_list(l):
     new_list = []
     for i in l:
         new_list+=i
-    x = "["
-    for i in new_list:
-        x+=str(i)
-        x+=","
-    x = x[:-1]+"]"
-    return x
+    
+    return new_list
